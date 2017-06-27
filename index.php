b7d31db8660ef43752cb6648668640b465367dbf
msav <msav@msav.ru>
2017-06-28 01:38:01 +0300

    init

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: msav
+ * Date: 27.06.2017
+ * Time: 23:19
+ */
+$test = "sdjkfh";
+echo $test;
+
+//phpinfo();
---
+++ tests/EmailTest.php
@@ -0,0 +1,35 @@
+<?php
+require_once ("../src/Email.php");
+/**
+ * Created by PhpStorm.
+ * User: msav
+ * Date: 28.06.2017
+ * Time: 0:15
+ */
+
+use PHPUnit\Framework\TestCase;
+
+final class EmailTest extends TestCase {
+	public function testCanBeCreatedFromValidEmailAddress()
+	{
+		$this->assertInstanceOf(
+			Email::class,
+			Email::fromString('user@example.com')
+		);
+	}
+
+	public function testCannotBeCreatedFromInvalidEmailAddress()
+	{
+		$this->expectException(InvalidArgumentException::class);
+
+		Email::fromString('invalid');
+	}
+
+	public function testCanBeUsedAsString()
+	{
+		$this->assertEquals(
+			'user@example.com',
+			Email::fromString('user@example.com')
+		);
+	}
+}
